9c65c84037dc2d8f43f9c923821c91c25fb20f9f
hiyamada <hiyamada@C010423685.zipangu.cac.co.jp>
2026-04-16 14:22:13 +0900

    ステージ6まで完了しました。

--- java_comprehension_exercises_volume1/src/basic/question06/Question06_2.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_2.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 public class Question06_2 {
 
 	public static void main(String[] args) throws IOException {
-
+		String[] studentName = { "かなこ", "ゆうた", "あい", "はるひ", "かずき" };
 		// TODO:ここに実装
-		
+		for (int i = 0; i < 5; i++) {
+			System.out.println(studentName[i]);
+		}
 	}
 }
